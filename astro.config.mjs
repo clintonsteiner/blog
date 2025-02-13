afbe672be8393b55927164ed5224704d43e5f8fb
CJ Steiner <clintonsteiner@gmail.com>
2025-02-13 02:15:54 -0600

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://clintonsteiner.com",
   integrations: [
     starlight({
       title: "Half Stack Python",
